ee114ef26cbaeaa6073dffd1dc96c83e236a5b2c
pavinduLakshan <174084F@uom.lk>
2019-05-22 22:57:05 +0530

    deleted task related files

--- src/main/java/com/itsfive/back/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itsfive.back.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.itsfive.back.model.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Integer>{
-
-}
