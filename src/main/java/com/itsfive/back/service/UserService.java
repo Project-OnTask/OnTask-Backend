6d5cb0e3a8fe43a3ad8331568ff5a0f4d6c255ab
pavinduLakshan <174084F@uom.lk>
2019-05-30 22:04:04 +0530

    implemented sending emails function

--- src/main/java/com/itsfive/back/service/UserService.java
+++ src/main/java/com/itsfive/back/service/UserService.java
@@ -2,27 +2,18 @@ package com.itsfive.back.service;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import com.itsfive.back.model.PasswordResetToken;
 import com.itsfive.back.model.User;
+import com.itsfive.back.repository.PasswordResetTokenRepository;
 import com.itsfive.back.repository.UserRepository;
 
 @Service
@@ -33,42 +24,19 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
     
-	public void sendmail(String email,String subject) throws AddressException, MessagingException, IOException {
-		   Properties props = new Properties();
-		   props.put("mail.smtp.auth", "true");
-		   props.put("mail.smtp.starttls.enable", "true");
-		   props.put("mail.smtp.host", "smtp.gmail.com");
-		   props.put("mail.smtp.port", "587");
-		   
-		   String MailPassword = env.getProperty("spring.mail.password");
-		   
-		   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-		      protected PasswordAuthentication getPasswordAuthentication() {
-		         return new PasswordAuthentication("plumberhl@gmail.com", MailPassword);
-		      }
-		   });
-		   Message msg = new MimeMessage(session);
-		   msg.setFrom(new InternetAddress("plumberhl@gmail.com", false));
-
-		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-		   msg.setSubject(subject);
-		   msg.setContent("Tutorials point email", "text/html");
-		   msg.setSentDate(new Date());
-
-		   MimeBodyPart messageBodyPart = new MimeBodyPart();
-		   messageBodyPart.setContent("Tutorials point email", "text/html");
-
-		   Multipart multipart = new MimeMultipart();
-		   multipart.addBodyPart(messageBodyPart);
-		   /*MimeBodyPart attachPart = new MimeBodyPart();
-
-		   attachPart.attachFile("/var/tmp/image19.png");
-		   multipart.addBodyPart(attachPart);
-		   msg.setContent(multipart);*/
-		   Transport.send(msg);   
-		}
+    @Autowired
+    private PasswordResetTokenRepository passwordResetTokenRepository;
 
 	public Optional<User> getUserById(Long id) {
 		return userRepository.findById(id);
+	}
+	
+	public Optional<User> getUserByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+	
+	public void createResetTokenForUser(User user,String token) {
+		PasswordResetToken passwordResetToken = new PasswordResetToken(user,token);
+		passwordResetTokenRepository.save(passwordResetToken);
 	}
 }
