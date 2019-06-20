672c85f95411f3ae0c2845f7b4cfb27f31d4e616
pavinduLakshan <174084F@uom.lk>
2019-06-20 11:33:46 +0530

    created file upload function

--- src/main/java/com/itsfive/back/NewBackendApplication.java
+++ src/main/java/com/itsfive/back/NewBackendApplication.java
@@ -3,8 +3,11 @@ package com.itsfive.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.itsfive.back.property.FileStorageProperties;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -13,6 +16,9 @@ import java.util.TimeZone;
 @EntityScan(basePackageClasses = { 
 		NewBackendApplication.class,
 		Jsr310JpaConverters.class 
+})
+@EnableConfigurationProperties({
+    FileStorageProperties.class
 })
 public class NewBackendApplication {
 	@PostConstruct
