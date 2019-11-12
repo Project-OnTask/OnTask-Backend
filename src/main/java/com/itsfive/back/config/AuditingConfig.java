d45ca9b6f16f6667aba4bd7836e6bb9caeacf65a
pavinduLakshan <174084F@uom.lk>
2019-11-13 01:09:07 +0530

    remove incorrect comment

--- src/main/java/com/itsfive/back/config/AuditingConfig.java
+++ src/main/java/com/itsfive/back/config/AuditingConfig.java
@@ -3,7 +3,6 @@ package com.itsfive.back.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//This configuration is used to generate createdAt and updatedAt fields in every table. 
 @Configuration
 @EnableJpaAuditing
 public class AuditingConfig {
